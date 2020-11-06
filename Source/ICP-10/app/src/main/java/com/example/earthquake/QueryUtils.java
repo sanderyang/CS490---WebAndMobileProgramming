eb89d75b6c946bd24d1147be91a7362c6f93fee3
Sandy <syy6v@mail.umkc.edu>
2020-11-05 21:41:03 -0600

    ICP 10

--- Source/ICP-10/app/src/main/java/com/example/earthquake/QueryUtils.java
+++ Source/ICP-10/app/src/main/java/com/example/earthquake/QueryUtils.java
@@ -1,9 +1,7 @@
 package com.example.earthquake;
 
-import android.os.Build;
 import android.util.Log;
 
-import androidx.annotation.RequiresApi;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,9 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,13 +36,12 @@ public class QueryUtils {
         //  URL object to store the url for a given string
         URL url = null;
         // A string to store the response obtained from rest call in the form of string
-        String jsonResponse = null;
-
+        String jsonResponse = "";
 
         //TODO: 1. Create a URL from the requestUrl string and make a GET request to it
         url = new URL(requestUrl);
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
+
+        HttpURLConnection urlConnection;
         try {
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setReadTimeout(10000);
@@ -53,27 +49,16 @@ public class QueryUtils {
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
 
-            if (urlConnection.getResponseCode() == 200) {
-                inputStream = urlConnection.getInputStream();
-
-                //TODO: 2. Read from the Url Connection and store it as a string(jsonResponse)
-                StringBuilder output = new StringBuilder();
-                if (inputStream != null) {
-                    InputStreamReader inputSR = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                    BufferedReader reader = new BufferedReader(inputSR);
-                    String line = reader.readLine();
-                    while (line != null) {
-                        output.append(line);
-                        line = reader.readLine();
-                    }
-                }
-                jsonResponse = output.toString();
-
-            } else {
-                Log.e(LOG_TAG, "Connection response code: " + urlConnection.getResponseCode());
+            //TODO: 2. Read from the Url Connection and store it as a string(jsonResponse)
+            if (urlConnection.getResponseCode() == 200){
+                jsonResponse = getInputData(url);
+                urlConnection.disconnect();
             }
-        }
-        catch (IOException e) {
+            else{
+                Log.e(LOG_TAG, "Connection Error: " + urlConnection.getResponseCode());
+            }
+
+        } catch (IOException e) {
             Log.e(LOG_TAG, "Cannot get JSON object.");
         }
 
@@ -102,12 +87,19 @@ public class QueryUtils {
         }catch (JSONException e){
             Log.e("QueryUtils", "Parsing problem", e);
         }
-        assert urlConnection != null;
-        urlConnection.disconnect();
-        assert inputStream != null;
-        inputStream.close();
 
         // Return the list of earthquakes
         return earthquakes;
+    }
+
+    private static String getInputData (URL earthquakeURL) throws IOException {
+        StringBuilder temp = new StringBuilder();
+        BufferedReader in = new BufferedReader(new InputStreamReader(earthquakeURL.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            temp.append(inputLine);
+        }
+        in.close();
+        return temp.toString();
     }
 }
